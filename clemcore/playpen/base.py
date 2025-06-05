1216d8b21cfd3ed325b53431810b9ba257a0ab51
dicaristic <dimitrije.ristic2112@outlook.com>
2025-06-05 18:56:01 +0200

    added no_grad()

--- clemcore/playpen/base.py
+++ clemcore/playpen/base.py
@@ -1,4 +1,5 @@
 import abc
+import torch
 
 from clemcore.backends import Model
 from clemcore.clemgame import GameRegistry
@@ -64,47 +65,48 @@ class BasePlayPenMultiturnTrajectory(BasePlayPen):
         retry_counter = 0
         retry_limit = 10
         while collected_trajectories < rollout_steps:
-            if retry_counter > retry_limit:
-                print('rollout terminated early!')
-                break
-            player, context = game_env.observe()
-            response = player(context)  # Returns a string - we don't want that
-            done, info = game_env.step(response)
+            with torch.no_grad():
+                if retry_counter > retry_limit:
+                    print('rollout terminated early!')
+                    break
+                player, context = game_env.observe()
+                response = player(context)  # Returns a string - we don't want that
+                done, info = game_env.step(response)
 
-            # Retrieve the full context for the turn
-            full_context = player.get_context()[:-1]  # Ensure this is unique for each step
-            response_dict = player.get_context()[-1:]  # Get the player response only. Return as list
+                # Retrieve the full context for the turn
+                full_context = player.get_context()[:-1]  # Ensure this is unique for each step
+                response_dict = player.get_context()[-1:]  # Get the player response only. Return as list
 
-            # Add to buffer only if the player's name matches `forPlayer`
-            if forPlayer in player.name:
-
-                rollout_buffer.on_step(
-                    context=full_context.copy() if isinstance(full_context, dict) else full_context[:],
-                    response=response_dict.copy(),
-                    done=done,
-                    info=info.copy() if isinstance(info, dict) else info[:]
-                )
-
-
-            # Check if the game is done (trajectory completed)
-            if game_env.is_done():
-                # Only collect the trajectory if the game ended on the desired player's turn
+                # Add to buffer only if the player's name matches `forPlayer`
                 if forPlayer in player.name:
-                    retry_counter = 0
-                    rollout_buffer.on_done()
-                    collected_trajectories += 1  # Increment trajectory count
-                    self.num_timesteps += 1
-                    self.callbacks.update_locals(locals())
-                    self.callbacks.on_step()
-                else:
-                    # Skip this trajectory if it ended on the other player's turn
-                    rollout_buffer.drop_trajectory()  # Clear the buffer for this trajectory
-                    print(f'Game end caused by other player. Dropping trajectory')
-                    retry_counter +=1
 
-                game_env.reset()
+                    rollout_buffer.on_step(
+                        context=full_context.copy() if isinstance(full_context, dict) else full_context[:],
+                        response=response_dict.copy(),
+                        done=done,
+                        info=info.copy() if isinstance(info, dict) else info[:]
+                    )
 
-        
+
+                # Check if the game is done (trajectory completed)
+                if game_env.is_done():
+                    # Only collect the trajectory if the game ended on the desired player's turn
+                    if forPlayer in player.name:
+                        retry_counter = 0
+                        rollout_buffer.on_done()
+                        collected_trajectories += 1  # Increment trajectory count
+                        self.num_timesteps += 1
+                        self.callbacks.update_locals(locals())
+                        self.callbacks.on_step()
+                    else:
+                        # Skip this trajectory if it ended on the other player's turn
+                        rollout_buffer.drop_trajectory()  # Clear the buffer for this trajectory
+                        print(f'Game end caused by other player. Dropping trajectory')
+                        retry_counter +=1
+
+                    game_env.reset()
+
+            
         # flatten the trajectories for further sampling.
         print('Rollout done - flattening trajectories')
         rollout_buffer.flatten_steps()
@@ -128,34 +130,34 @@ class BasePlayPenMultiturn(BasePlayPen):
 
         num_rollout_steps = 0
         while num_rollout_steps < rollout_steps:
+            with torch.no_grad():
+                player, context = game_env.observe()
+                response = player(context) # returns a string - we don't want that
+                done, info = game_env.step(response)
 
-            player, context = game_env.observe()
-            response = player(context) # returns a string - we don't want that
-            done, info = game_env.step(response)
+                # Retrieve the full context for the turn
+                full_context = player.get_context()[:-1]  # Ensure this is unique for each step
+                response_dict = player.get_context()[-1:] # get the player response only. Return as list
+                # Add to buffer only if the player's name matches `forPlayer`
+                if forPlayer in player.name:
 
-            # Retrieve the full context for the turn
-            full_context = player.get_context()[:-1]  # Ensure this is unique for each step
-            response_dict = player.get_context()[-1:] # get the player response only. Return as list
-            # Add to buffer only if the player's name matches `forPlayer`
-            if forPlayer in player.name:
+                    self.num_timesteps += 1
+                    # only count rollout step if it's for the player we are trainign? 
+                    num_rollout_steps += 1
 
-                self.num_timesteps += 1
-                # only count rollout step if it's for the player we are trainign? 
-                num_rollout_steps += 1
+                    rollout_buffer.on_step(
+                        context=full_context.copy() if isinstance(full_context, dict) else full_context[:],
+                        response=response_dict.copy(),
+                        done=done,
+                        info=info.copy() if isinstance(info, dict) else info[:]
+                    )
 
-                rollout_buffer.on_step(
-                    context=full_context.copy() if isinstance(full_context, dict) else full_context[:],
-                    response=response_dict.copy(),
-                    done=done,
-                    info=info.copy() if isinstance(info, dict) else info[:]
-                )
+                self.callbacks.update_locals(locals())
+                self.callbacks.on_step()
 
-            self.callbacks.update_locals(locals())
-            self.callbacks.on_step()
-
-            if game_env.is_done():
-                rollout_buffer.on_done()
-                game_env.reset()
+                if game_env.is_done():
+                    rollout_buffer.on_done()
+                    game_env.reset()
 
         # Notify callbacks that rollout has ended
         self.callbacks.on_rollout_end()
